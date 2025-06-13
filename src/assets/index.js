b23b7673e301905b7a6ee781d66d8aea8b71e98a
Azedine Chentouf <148768155+Azedize@users.noreply.github.com>
2025-06-13 12:35:24 +0100

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,7 @@
 import logo from "./images/logo1.jpg";
 import bannerImg from "./images/potfolio.png";
+import bannerImg1 from "./images/potfolio.png";
+
 import homme from "./images/projects/AS-Homme.png";
 import calc from "./images/projects/Calculatrice.png";
 import p1 from "./images/projects/P1.png";
@@ -27,4 +29,5 @@ export {
   testimonialTwo,
   quote,
   contactImg,
+  bannerImg1
 };
